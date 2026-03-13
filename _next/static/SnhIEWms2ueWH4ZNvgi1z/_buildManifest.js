39cb3337a3012573947ae4ab5d72ba4319ffd3e3
Yousef Altaher <yousef.a.altaher@gmail.com>
2026-03-13 15:41:28 +0300

    Updates

---
+++ _next/static/SnhIEWms2ueWH4ZNvgi1z/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/qnn-noise//_next/:path+",
+        "destination": "/qnn-noise/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
