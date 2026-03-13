c4f62b37a04ec93b49c7aa9455324ca902a799f2
Yousef Altaher <yousef.a.altaher@gmail.com>
2026-03-13 15:30:21 +0300

    GH Page support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+const repoName = 'qnn-noise';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: isProduction ? `/${repoName}` : '',
+  assetPrefix: isProduction ? `/${repoName}/` : '',
   turbopack: {
     resolveAlias: {
       fs: { browser: './empty.js' },
